a4b0512064341823ab0d5e7bf3920c63acf8c25a
viniagostini <fodas123>
2016-11-30 01:02:38 -0200

    progress bar done and bug fix on delete feature

--- components/toDoList/toDoListCtlr.js
+++ components/toDoList/toDoListCtlr.js
@@ -1,15 +1,16 @@
 angular.module('toDoList').controller('toDoListCtlr', ['$scope' , function($scope) {
 
   $scope.title = "To Do List";
+  var taskIDAux = 0;
+  $scope.tasks = [];
+
 
   function Task (id, title){
-    this.id = id;
+    this.id = taskIDAux++;
     this.title = title;
     this.createdAt = new Date();
     this.isDone = false;
   }
-
-  $scope.tasks = [];
 
   function fillTasks(){
     var task1 = new Task($scope.tasks.length, "Comemorar titulo do Palmeiras");
@@ -23,18 +24,52 @@ angular.module('toDoList').controller('toDoListCtlr', ['$scope' , function($scop
     $scope.newTaskTitle = '';
   }
 
-  $scope.defineStyle = function(isDone){
+  $scope.defineTaskStyle = function(isDone){
     if(isDone){
       return 'line-through';
     }
   }
 
-  $scope.deleteTask = function(id){
-      $scope.tasks.splice( id, 1 );
+  $scope.defineProgressBarStyle = function(tasks){
+    var doneTasksPercetage = _getDoneTasksPercetage(tasks);
+    var DEFAULT_CLASS = 'progress-bar progress-bar-striped active';
+
+    if(doneTasksPercetage >= 75){
+      return DEFAULT_CLASS + ' progress-bar-success';
+    }else if(doneTasksPercetage >= 50){
+      return DEFAULT_CLASS + ' progress-bar-info';
+    }else if(doneTasksPercetage >= 25){
+      return DEFAULT_CLASS + ' progress-bar-warning';
+    }else{
+      return DEFAULT_CLASS + ' progress-bar-danger';
+    }
+  }
+
+  $scope.getDoneTasksPercetage = function(tasks){
+    return _getDoneTasksPercetage(tasks);
+  }
+
+
+  function _getDoneTasksPercetage(tasks){
+    var nTasksDone = 0;
+    tasks.forEach(function(task){
+      if(task.isDone){
+        nTasksDone++;
+      }
+    });
+    return (nTasksDone / tasks.length) * 100
+  }
+
+  $scope.deleteTask = function(tasks, id){
+    $scope.tasks = tasks.filter(function (task){
+      if(task.id != id) return task;
+    });
+    //$scope.tasks.splice( id, 1 );
   }
 
 
   fillTasks();
+
 
 
 }]);
